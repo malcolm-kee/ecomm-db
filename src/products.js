002956149133f4ab3c533d63747b8c47715dbbd1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-03-14 01:53:26 +0800

    update default products

--- src/products.js
+++ src/products.js
@@ -3,24 +3,32 @@ module.exports = [
     id: 1,
     name: 'iPhone X',
     descriptions: ['user-friendly', 'change the world', 'cool', 'expensive'],
-    image: 'iphone-x.jpg'
+    image: 'iphone-x.jpg',
+    department: 'Electronics',
+    price: '3499.00'
   },
   {
     id: 2,
     name: 'Samsung Galaxy S10',
     descriptions: ['big', 'heavy', 'expensive'],
-    image: 'samsung-galaxy-s10.jpg'
+    image: 'samsung-galaxy-s10.jpg',
+    department: 'Electronics',
+    price: '3299.00'
   },
   {
     id: 3,
     name: 'Daniel Wellington Classic Cambridge',
     descriptions: ['pretty', 'elegent', 'expensive'],
-    image: 'dw-watch.png'
+    image: 'dw-watch.png',
+    department: 'Clothing',
+    price: '1999.00'
   },
   {
     id: 4,
     name: 'dodo',
     descriptions: [],
-    image: 'dono.png'
+    image: 'dono.png',
+    department: 'Food',
+    price: '2.50'
   }
 ];
