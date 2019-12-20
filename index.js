656f6dc37fa78e52bc2a126ed8e7f50d9ff32a78
malcolm-kee <malcolm.keeweesiong@gmail.com>
2019-12-20 10:30:10 +0800

    add chat app

--- index.js
+++ index.js
@@ -1,74 +1,60 @@
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 const jsonServer = require('json-server');
 const express = require('express');
-// const formidableMiddleware = require('express-formidable');
+const formidableMiddleware = require('express-formidable');
 const server = express();
 const expressWs = require('express-ws')(server);
-// const middlewares = jsonServer.defaults({
-//   static: path.join(__dirname, 'build', 'public'),
-// });
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, 'build', 'public'),
+});
 
-// const APP_NAME = process.env.HEROKU_APP_NAME;
+const APP_NAME = process.env.HEROKU_APP_NAME;
 const PORT = process.env.PORT || 6366;
-// const app_base = APP_NAME ? `https://${APP_NAME}.herokuapp.com` : `http://localhost:${PORT}`;
+const app_base = APP_NAME ? `https://${APP_NAME}.herokuapp.com` : `http://localhost:${PORT}`;
 
-// const uploadFolder = path.resolve(__dirname, 'build', 'public', 'uploads');
+const uploadFolder = path.resolve(__dirname, 'build', 'public', 'uploads');
 
-// if (!fs.existsSync(uploadFolder)) {
-//   fs.mkdirSync(uploadFolder);
-// }
+if (!fs.existsSync(uploadFolder)) {
+  fs.mkdirSync(uploadFolder);
+}
 
-// server.use(middlewares);
-// server.put(
-//   '/upload',
-//   formidableMiddleware({
-//     uploadDir: uploadFolder,
-//     keepExtensions: true,
-//     multiples: true,
-//   }),
-//   (req, res) => {
-//     return res.json({
-//       files: Object.values(req.files).map(file => {
-//         const { name, ext } = path.parse(file.path);
-//         return `${app_base}/uploads/${name}${ext}`;
-//       }),
-//     });
-//   }
-// );
+server.use(middlewares);
+server.put(
+  '/upload',
+  formidableMiddleware({
+    uploadDir: uploadFolder,
+    keepExtensions: true,
+    multiples: true,
+  }),
+  (req, res) => {
+    return res.json({
+      files: Object.values(req.files).map(file => {
+        const { name, ext } = path.parse(file.path);
+        return `${app_base}/uploads/${name}${ext}`;
+      }),
+    });
+  }
+);
 
 server.use('/api', jsonServer.router(path.join(__dirname, 'build', 'db.json')));
 
-server.use(express.static(path.resolve(__dirname, 'build', 'public')));
-
-server.ws('/', ws => {
-  ws.on('message', () => {
-    console.log(`Socket client msg`);
+server.ws('/chat', ws => {
+  ws.on('message', msg => {
+    broadcast(msg);
   });
 
   ws.on('open', () => console.log(`New connection`));
 
   ws.on('close', () => console.log(`Socket client disconnected`));
-
-  console.log('socket');
 });
 
-const wss = expressWs.getWss();
 const broadcast = msg => {
   expressWs.getWss().clients.forEach(client => {
     client.send(msg);
   });
 };
 
-wss.on('connection', ws => {
-  console.log('client connected');
-  ws.on('close', () => console.log('client disconnected in wss'));
-});
-
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-setInterval(() => {
-  broadcast(new Date().toTimeString());
-}, 1000);
